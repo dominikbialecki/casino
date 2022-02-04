04a6d47bc2cd20c07fa9ce387eb99a868c22a8ad
Dominik Białecki <dominik.bialecki-extern@dbschenker.com>
2022-02-04 07:51:04 +0100

    test navbar displays links

--- src/app/ui/ui-navbar/ui-navbar.component.spec.ts
+++ src/app/ui/ui-navbar/ui-navbar.component.spec.ts
@@ -1,4 +1,7 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { UiNavbarItem } from './ui-navbar-item';
 
 import { UiNavbarComponent } from './ui-navbar.component';
 
@@ -8,7 +11,8 @@ describe('UiNavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UiNavbarComponent]
+      declarations: [UiNavbarComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
   });
@@ -16,10 +20,32 @@ describe('UiNavbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UiNavbarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should display a link for each provided item', () => {
+    // given I have navbar items
+    const ITEMS: UiNavbarItem[] = [
+      { label: 'label1', redirectUrl: 'link1' },
+      { label: 'label2', redirectUrl: 'link2' },
+      { label: 'label3', redirectUrl: 'link3' }
+    ];
+    component.items = ITEMS;
+
+    // when I provide items
+    component.items = ITEMS;
+    fixture.detectChanges();
+
+    // then links were displayed
+    const links = fixture.debugElement.queryAll(By.css('a'));
+    expect(links).toHaveSize(ITEMS.length);
+    links.forEach((linkDebugElement, index) => {
+      expect(linkDebugElement.nativeElement.text).toContain(ITEMS[index].label);
+    });
+
   });
 });
